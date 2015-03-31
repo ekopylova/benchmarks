bc5bcc5b53164036e432fea25973f62bda459fe5
Jenya Kopylova <jenya.kopylov@gmail.com>
2015-03-31 14:01:16 -0600

    add blast as observed alignments option

--- suppl_compute_accuracy.py
+++ suppl_compute_accuracy.py
@@ -39,7 +39,7 @@ def collect_ground_truth(ground_truth_alns_fp):
     return expected_alns
 
 
-def collect_observed_alignments(observed_alns_fp, format="sam"):
+def collect_observed_alignments(observed_alns_fp, file_format="sam"):
     """Parses a file of observed alignments into a dictionary
        (BLAST or SAM)
 
@@ -47,7 +47,7 @@ def collect_observed_alignments(observed_alns_fp, format="sam"):
        -----------
        observed_alns_fp : string
           filepath to observed alignments
-       format : string
+       file_format : string
           file format of alignments (blast or sam)
 
        Returns:
@@ -58,23 +58,27 @@ def collect_observed_alignments(observed_alns_fp, format="sam"):
     observed_alns = {}
 
     with open(observed_alns_fp, 'U') as observed_alns_f:
-        if format == "sam":
+        if file_format == "sam":
             for line in observed_alns_f:
                 if line.startswith('@'):
                     continue
                 line = line.strip().split('\t')
-                contig = line[2].split()[0]
+                contig = line[2].split()
+                if contig:
+                    contig = contig[0]
                 # no alignment found for this read
                 if contig == "*":
                     continue
 
-                read_id = line[0]
+                read_id = line[0].split()
+                if read_id:
+                    read_id = read_id[0]
                 if read_id not in observed_alns:
                     observed_alns[read_id] = line[1:]
                 else:
                     raise ValueError("Only 1 alignment per read: %s" % read_id)
 
-        elif format == "blast":
+        elif file_format == "blast":
             for line in observed_alns_f:
                 line = line.strip().split('\t')
                 contig = line[1]
@@ -83,11 +87,13 @@ def collect_observed_alignments(observed_alns_fp, format="sam"):
                     observed_alns[read_id] = line[1:]
                 else:
                     raise ValueError("Only 1 alignment per read: %s" % read_id)
+        else:
+            raise ValueError("%s file format not supported" % file_format)
 
     return observed_alns
 
 
-def compute_accuracy(expected_alns, observed_alns, format="sam", offset=0):
+def compute_accuracy(expected_alns, observed_alns, file_format="sam", offset=0):
     """For each observed alignment, compute the accuracy score based on the list of
        expected alignments. The accuracy score is between [0,1] and is weighted based
        on the quality of the alignment given by the bit score.
@@ -110,8 +116,8 @@ def compute_accuracy(expected_alns, observed_alns, format="sam", offset=0):
        observed_alns : dictionary
           dictionary of observed alignments, keys are read ids and values are
           a list of SAM or BLAST alignments
-       format : string, optional
-          format of observed alignments (SAM or BLAST)
+       file_format : string, optional
+          file format of observed alignments (SAM or BLAST)
        offset : integer, optional
           the maximum difference between the expected alignment position and
           the observed
@@ -128,39 +134,44 @@ def compute_accuracy(expected_alns, observed_alns, format="sam", offset=0):
     false_positive = set()
     true_positive = set()
 
-    if format == "sam":
-        for read_id in observed_alns:
-            if read_id not in expected_alns:
-                #print "WARNING: alignment %s in observed but not expected" % read_id
-                continue
-            obs_contig = observed_alns[read_id][1]
-            obs_pos = int(observed_alns[read_id][2])
-            # compute the total number of unique bitscores in list of expected alignments
-            unique_bitscores = set()
-            for exp_aln in expected_alns[read_id]:
-                unique_bitscores.add(exp_aln[10])
-            num_unique_bitscores = float(len(unique_bitscores))
-            # find the expected alignment for this read
-            index = 0
-            weight = num_unique_bitscores
-            for exp_aln in expected_alns[read_id]:
-                exp_contig = exp_aln[0]
-                exp_pos = int(exp_aln[7])
-                # alignment found, compute accuracy score and go to next read
-                if ((obs_contig == exp_contig) and (abs(obs_pos - exp_pos) <= offset)):
-                    accuracy_score = float(weight/num_unique_bitscores)
-                    all_accuracy_scores.append(accuracy_score)
-                    break
-                if index < len(expected_alns[read_id]) - 1:
-                    # decrement the weight
-                    if float(expected_alns[read_id][index+1][10]) < float(exp_aln[10]):
-                        weight -= 1
-                index += 1
+    if file_format == "sam":
+        obs_contig_index = 1
+        obs_pos_index = 2
+    elif file_format == "blast":
+        obs_contig_index = 0
+        obs_pos_index = 7
+    else:
+        raise ValueError("%s file format not supported" % file_format)
 
-        total_accuracy_score = float(sum(all_accuracy_scores)/len(observed_alns))
+    for read_id in observed_alns:
+        if read_id not in expected_alns:
+            #print "WARNING: alignment %s in observed but not expected" % read_id
+            continue
+        obs_contig = observed_alns[read_id][obs_contig_index]
+        obs_pos = int(observed_alns[read_id][obs_pos_index])
+        # compute the total number of unique bitscores in list of expected alignments
+        unique_bitscores = set()
+        for exp_aln in expected_alns[read_id]:
+            unique_bitscores.add(exp_aln[10])
+        num_unique_bitscores = float(len(unique_bitscores))
+        # find the expected alignment for this read
+        index = 0
+        weight = num_unique_bitscores
+        for exp_aln in expected_alns[read_id]:
+            exp_contig = exp_aln[0]
+            exp_pos = int(exp_aln[7])
+            # alignment found, compute accuracy score and go to next read
+            if ((obs_contig == exp_contig) and (abs(obs_pos - exp_pos) <= offset)):
+                accuracy_score = float(weight/num_unique_bitscores)
+                all_accuracy_scores.append(accuracy_score)
+                break
+            if index < len(expected_alns[read_id]) - 1:
+                # decrement the weight
+                if float(expected_alns[read_id][index+1][10]) < float(exp_aln[10]):
+                    weight -= 1
+            index += 1
 
-    elif format == "blast":
-
+    total_accuracy_score = float(sum(all_accuracy_scores)/len(observed_alns))
         
     return total_accuracy_score*100.0
 
@@ -205,7 +216,7 @@ def _main(expected_alns_fp, observed_alns_fp, tool, observed_aln_format, total_r
         raise ValueError("%s is not supported" % observed_aln_format)
 
     expected_alns = collect_ground_truth(ground_truth_alns_fp=expected_alns_fp)
-    observed_alns = collect_observed_alignments(observed_alns_fp=observed_alns_fp, format=observed_aln_format)
+    observed_alns = collect_observed_alignments(observed_alns_fp=observed_alns_fp, file_format=observed_aln_format)
     accuracy = compute_accuracy(expected_alns, observed_alns)
 
     tp, fp, fn, p, r, f = compute_precision(expected_alns, observed_alns)
--- test_suppl_compute_accuracy.py
+++ test_suppl_compute_accuracy.py
@@ -38,15 +38,23 @@ class SupplComputeAccuracyTests(TestCase):
         with open(self.exp_alns_1_fp, 'w') as tmp:
             tmp.write(expected_alignments_1)
 
-        # write observed_alignments_1 to temporary file
-        f, self.obs_alns_1_fp = mkstemp(prefix='obs_alns_1_',
-                                        suffix='.sam')
+        # write observed_sam_alignments_1 to temporary file
+        f, self.obs_sam_alns_1_fp = mkstemp(prefix='obs_alns_1_',
+                                            suffix='.sam')
         close(f)
-        with open(self.obs_alns_1_fp, 'w') as tmp:
-            tmp.write(observed_alignments_1)
+        with open(self.obs_sam_alns_1_fp, 'w') as tmp:
+            tmp.write(observed_sam_alignments_1)
+
+        # write observed_blast_alignments_1 to temporary file
+        f, self.obs_blast_alns_1_fp = mkstemp(prefix='obs_alns_1_',
+                                              suffix='.blast')
+        close(f)
+        with open(self.obs_blast_alns_1_fp, 'w') as tmp:
+            tmp.write(observed_blast_alignments_1)
 
         self.files_to_remove = [self.exp_alns_1_fp,
-                                self.obs_alns_1_fp]
+                                self.obs_sam_alns_1_fp,
+                                self.obs_blast_alns_1_fp]
 
     def tearDown(self):
         remove_files(self.files_to_remove)
@@ -80,7 +88,7 @@ class SupplComputeAccuracyTests(TestCase):
             # list of lists must be sorted by decreasing bit score
             self.assertEqual(exp_alns[aln], given_alns[aln])
 
-    def test_collect_observed_alignments(self):
+    def test_collect_observed_sam_alignments(self):
         """
         """
         exp_sam_alns = {
@@ -91,7 +99,7 @@ class SupplComputeAccuracyTests(TestCase):
             'seq.000001004':
             ['0', 'ref1', '525642', '255', '150M', '*', '0', '0', 'AAGATGAGAATTCTAAGGTGAGCGAGCGAACTCTCGTTAAGGAACTCGGCAAAATGACCCCGTAACTTCGGGAGAAGGGGTGCTCTTTAGGGTTAACGCCCAGAAGAGCCGCAGTGAATAGGCCCAAGCGACTGTTTATCAAACACACAG', '*', 'AS:i:295', 'NM:i:1']}
 
-        obs_alns = collect_observed_alignments(self.obs_alns_1_fp)
+        obs_alns = collect_observed_alignments(self.obs_sam_alns_1_fp)
 
         self.assertEqual(len(exp_sam_alns), len(obs_alns))
 
@@ -99,10 +107,29 @@ class SupplComputeAccuracyTests(TestCase):
             self.assertTrue(aln in exp_sam_alns)
             self.assertEqual(exp_sam_alns[aln], obs_alns[aln])
 
-    def test_compute_accuracy_100(self):
+    def test_collect_observed_blast_alignments(self):
+        """
+        """
+        exp_blast_alns = {
+        'seq.000000828': ['ref1', '96.34', '82', '3', '0', '69', '150', '2428038', '2428119', '1e-32', '134'],
+        'seq.000001026': ['ref1', '98.00', '150', '0', '1', '1', '150', '524183', '524329', '4e-69', '255'],
+        'seq.000001004': ['ref1', '99.33', '150', '1', '0', '1', '150', '525642', '525791', '2e-72', '266']
+        }
+
+        obs_alns = collect_observed_alignments(observed_alns_fp=self.obs_blast_alns_1_fp,
+                                               file_format="blast")
+
+        self.assertEqual(len(exp_blast_alns), len(obs_alns))
+
+        for aln in obs_alns:
+            self.assertTrue(aln in exp_blast_alns)
+            self.assertEqual(exp_blast_alns[aln], obs_alns[aln])      
+
+    def test_compute_accuracy_100_sam(self):
         """ Given a set of expected alignments, each observed alignment
             should achieve an accuracy score of 100.0%, thus a total of
             100.0% accuracy score for all observed alignments
+            (observed alignments in SAM format)
         """
         exp_alns = {
             'seq.000000828': [
@@ -133,11 +160,45 @@ class SupplComputeAccuracyTests(TestCase):
 
         self.assertEqual(accuracy, 100.0)
 
-    def test_compute_accuracy_72(self):
+    def test_compute_accuracy_100_blast(self):
+        """ Given a set of expected alignments, each observed alignment
+            should achieve an accuracy score of 100.0%, thus a total of
+            100.0% accuracy score for all observed alignments
+            (observed alignments in BLAST format)
+        """
+        exp_alns = {
+            'seq.000000828': [
+            ['ref1', '96.34', '82', '3', '0', '69', '150', '2428038', '2428119', '9.5e-26', '111.8'],
+            ['ref1', '98.61', '72', '1', '0', '1', '72', '2426641', '2426712', '1.6e-23', '104.5']],
+            'seq.000001026': [
+            ['ref1', '98.00', '150', '0', '3', '1', '150', '524183', '524329', '3.5e-69', '256.1'],
+            ['ref1', '97.33', '150', '1', '3', '1', '150', '568163', '568309', '8.1e-68', '251.6'],
+            ['ref1', '97.33', '150', '1', '3', '150', '1', '2292122', '2292268', '8.1e-68', '251.6'],
+            ['ref1', '97.33', '150', '1', '3', '150', '1', '2174250', '2174396', '8.1e-68', '251.6'],
+            ['ref1', '96.67', '150', '2', '3', '150', '1', '1978335', '1978481', '1.8e-66', '247.1']],
+            'seq.000001004': [
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '2290660', '2290809', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '1976873', '1977022', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '1', '150', '525642', '525791', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '1', '150', '569622', '569771', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '2172788', '2172937', '3.9e-74', '272.6']]}
+
+        obs_alns = {
+        'seq.000000828': ['ref1', '96.34', '82', '3', '0', '69', '150', '2428038', '2428119', '1e-32', '134'],
+        'seq.000001026': ['ref1', '98.00', '150', '0', '1', '1', '150', '524183', '524329', '4e-69', '255'],
+        'seq.000001004': ['ref1', '99.33', '150', '1', '0', '1', '150', '525642', '525791', '2e-72', '266']
+        }
+
+        accuracy = compute_accuracy(exp_alns, obs_alns, file_format="blast")
+
+        self.assertEqual(accuracy, 100.0)
+
+    def test_compute_accuracy_72_sam(self):
         """ Given a set of expected alignments, 2/3 observed alignments
             are not optimal with seq.000000828 & seq.000001026 being second best,
             and seq.000001004 being best.
             The final accuracy score is (1/2 + 2/3 + 1)/3 = 0.722
+            (observed alignments in SAM format)
         """
         exp_alns = {
             'seq.000000828': [
@@ -165,6 +226,42 @@ class SupplComputeAccuracyTests(TestCase):
             ['0', 'ref1', '525642', '255', '150M', '*', '0', '0', 'AAGATGAGAATTCTAAGGTGAGCGAGCGAACTCTCGTTAAGGAACTCGGCAAAATGACCCCGTAACTTCGGGAGAAGGGGTGCTCTTTAGGGTTAACGCCCAGAAGAGCCGCAGTGAATAGGCCCAAGCGACTGTTTATCAAACACACAG', '*', 'AS:i:295', 'NM:i:1']}
 
         accuracy = compute_accuracy(exp_alns, obs_alns)
+
+        self.assertEqual(float("%0.1f" % accuracy), 72.2)
+
+    def test_compute_accuracy_72_blast(self):
+        """ Given a set of expected alignments, 2/3 observed alignments
+            are not optimal with seq.000000828 & seq.000001026 being second best,
+            and seq.000001004 being best.
+            The final accuracy score is (1/2 + 2/3 + 1)/3 = 0.722
+            (observed alignments in BLAST format)
+        """
+        exp_alns = {
+            'seq.000000828': [
+            ['ref1', '96.34', '82', '3', '0', '69', '150', '2428038', '2428119', '9.5e-26', '111.8'],
+            ['ref1', '98.61', '72', '1', '0', '1', '72', '2426641', '2426712', '1.6e-23', '104.5']],
+            'seq.000001026': [
+            ['ref1', '98.00', '150', '0', '3', '1', '150', '524183', '524329', '3.5e-69', '256.1'],
+            ['ref1', '97.33', '150', '1', '3', '1', '150', '568163', '568309', '8.1e-68', '251.6'],
+            ['ref1', '97.33', '150', '1', '3', '150', '1', '2292122', '2292268', '8.1e-68', '251.6'],
+            ['ref1', '97.33', '150', '1', '3', '150', '1', '2174250', '2174396', '8.1e-68', '251.6'],
+            ['ref1', '96.67', '150', '2', '3', '150', '1', '1978335', '1978481', '1.8e-66', '247.1']],
+            'seq.000001004': [
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '2290660', '2290809', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '1976873', '1977022', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '1', '150', '525642', '525791', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '1', '150', '569622', '569771', '3.9e-74', '272.6'],
+            ['ref1', '99.33', '150', '1', '0', '150', '1', '2172788', '2172937', '3.9e-74', '272.6']]}
+
+        obs_alns = {
+            'seq.000000828':
+            ['ref1', '98.61', '72', '1', '0', '1', '72', '2426641', '2426712', '5e-30', '125'],
+            'seq.000001026':
+            ['ref1', '97.33', '150', '1', '1', '1', '150', '568163', '568309', '5e-68', '251'],
+            'seq.000001004':
+            ['ref1', '99.33', '150', '1', '0',  '1', '150', '525642', '525791', '2e-72', '266']}
+
+        accuracy = compute_accuracy(exp_alns, obs_alns, file_format="blast")
 
         self.assertEqual(float("%0.1f" % accuracy), 72.2)
 
@@ -264,11 +361,16 @@ seq.000001026\tref1\t97.33\t150\t1\t3\t150\t1\t2292122\t2292268\t8.1e-68\t251.6
 seq.000001026\tref1\t97.33\t150\t1\t3\t150\t1\t2174250\t2174396\t8.1e-68\t251.6
 """
 
-observed_alignments_1 = """@HD  VN:1.0  SO:unsorted
+observed_sam_alignments_1 = """@HD  VN:1.0  SO:unsorted
 @PG ID:program1    VN:1.0  CL:test1
 seq.000000828\t0\tref1\t2428038\t255\t68S82M\t*\t0\t0\tGATTAATTTATAAAAATCAAATCAGGCATTAAATAAAATAGCCCATAAATACAAAGTGTTATCACCTTCTATTTAGGGGCTATTAGTTCTATTCGTCATTCTTTTTACAGATCATTCTATCTAATTAATTTGTGTACAATTTTGATAACT\t*\tAS:i:149\tNM:i:3
 seq.000001004\t0\tref1\t525642\t255\t150M\t*\t0\t0\tAAGATGAGAATTCTAAGGTGAGCGAGCGAACTCTCGTTAAGGAACTCGGCAAAATGACCCCGTAACTTCGGGAGAAGGGGTGCTCTTTAGGGTTAACGCCCAGAAGAGCCGCAGTGAATAGGCCCAAGCGACTGTTTATCAAACACACAG\t*\tAS:i:295\tNM:i:1
 seq.000001026\t0\tref1\t524183\t255\t110M3I37M\t*\t0\t0\tCCCGGAGGAAGAGAAAGAAAATTCGATTCCCTTAGTAGCGGCGAGCGAAATGGGAAGAGCCCAAACCAACAAGCTTGCTTGTTGGGGTTGTAGGACACTCTATACGGAGTCTCTACAAAGGACGACATTAGACGAATCATCTGGAAAGAT\t*\tAS:i:285\tNM:i:3
+"""
+
+observed_blast_alignments_1 = """seq.000000828\tref1\t96.34\t82\t3\t0\t69\t150\t2428038\t2428119\t1e-32\t134
+seq.000001026\tref1\t98.00\t150\t0\t1\t1\t150\t524183\t524329\t4e-69\t255
+seq.000001004\tref1\t99.33\t150\t1\t0\t1\t150\t525642\t525791\t2e-72\t266
 """
 
 if __name__ == '__main__':
